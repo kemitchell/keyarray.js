e980c96d4eb7b52d64ba8c82f71bca630e819c9e
Kyle Mitchell <kyle@kemitchell.com>
2015-04-13 16:30:25 -0700

    wrap keyarray-set

--- keyarray.js
+++ keyarray.js
@@ -1,5 +1,6 @@
 exports.delete = require('keyarray-delete');
 exports.get = require('keyarray-get');
 exports.has = require('keyarray-has');
+exports.set = require('keyarray-set');
 
 exports.version = '0.1.1';
