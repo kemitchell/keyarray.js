f29d590dcc7d62ddb4d8bd726a6f090e0660486b
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-24 01:37:59 -0700

    Remove version string property

--- keyarray.js
+++ keyarray.js
@@ -2,5 +2,3 @@ exports.delete = require('keyarray-delete');
 exports.get = require('keyarray-get');
 exports.has = require('keyarray-has');
 exports.set = require('keyarray-set');
-
-exports.version = '0.1.2';
